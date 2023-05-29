fcba8aec70e29240d37332eac69863302778faa8
Aditya Bodewar <98313615+madbunny253@users.noreply.github.com>
2023-05-29 11:20:34 +0530

    Update lineclipping.c

--- lineclipping.c
+++ lineclipping.c
@@ -1,237 +1,244 @@
-#include <stdio.h>
-#include <math.h>
-#include <GL/glut.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<GL/glut.h>
+float xa , ya ,xb ,yb;
+float slope;
+float org_xa,org_ya,org_xb,org_yb;
+float xmin ,ymin ,xmax,ymax;
+int outcode1[4],outcode2[4];
 
-int xmin, xmax, ymin, ymax;
-int code[4];
-
-void test();
-
-struct LineClipping
+int Round(float a)
 {
-    float x, y;
-    int binary[4];
-} p1, p2;
-
-void LineDDA(int xa, int ya, int xb, int yb)
-{
-    int dx = (xb - xa);
-    int dy = (yb - ya);
-    int steps;
-    float xInc, yInc, x = xa, y = ya;
-
-    steps = (abs(dx) > abs(dy)) ? (abs(dx)) : (abs(dy));
-    xInc = dx / (float)steps;
-    yInc = dy / (float)steps;
-
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-
-    int k;
-    for (k = 0; k < steps; k++)
-    {
-        x += xInc;
-        y += yInc;
-        glVertex2i(floor(x + 0.5), floor(y + 0.5));
-    }
-
-    glEnd();
-    glFlush();
+      return (int)(a+0.5);
 }
-
-void check(struct LineClipping *p)
+     
+void ddaline(float xa,float ya,float xb,float yb)
 {
-    if (p->x < xmin)
-        p->binary[3] = 1;
-    else
-        p->binary[3] = 0;
-
-    if (p->x > xmax)
-        p->binary[2] = 1;
-    else
-        p->binary[2] = 0;
-
-    if (p->y < ymin)
-        p->binary[1] = 1;
-    else
-        p->binary[1] = 0;
-
-    if (p->y > ymax)
-        p->binary[0] = 1;
-    else
-        p->binary[0] = 0;
-}
-
-void calculate(struct LineClipping *p)
-{
-    int i, flag;
-    float x, y;
-
-    for (i = 0; i < 4; i++)
-    {
-        if (p->binary[i] == 0)
-            flag = 0;
-        else
+        int dx,dy,steps;
+        dx = xb-xa;
+        dy = yb-ya;
+        if(abs(dx)>abs(dy))
         {
-            flag = 1;
-            break;
-        }
-    }
-
-    if (flag == 0)
-        printf("\nNo intersection");
-    else
-    {
-        if (p->binary[3] == 1)
-            x = xmin;
-        if (p->binary[2] == 1)
-            x = xmax;
-        if (p->binary[1] == 1)
-            y = ymin;
-        if (p->binary[0] == 1)
-            y = ymax;
-
-        if (p->binary[3] == 1 || p->binary[2] == 1)
-        {
-            float m = (p2.y - p1.y) / (float)(p2.x - p1.x);
-            y = p->y + m * (x - p->x);
+        	steps = abs(dx);
         }
         else
         {
-            float m = (p2.y - p1.y) / (float)(p2.x - p1.x);
-            x = p->x + (y - p->y) / m;
+         	steps = abs(dy);
         }
 
-        p->x = x;
-        p->y = y;
-    }
+
+     	float xincrement , yincrement;
+     	xincrement = dx/(float)(steps);
+     	yincrement = dy/(float)(steps);
+     	float x = xa;
+     	float y = ya;
+
+       glVertex2d(Round(x),Round(y));
+
+      for(int k = 0; k<steps; k++)
+     {
+	      x=x+xincrement;
+	      y=y+yincrement;
+	      glVertex2d(Round(x),Round(y));
+     }
 }
 
-void find()
+void Draw()
 {
-    calculate(&p1);
-    printf("\n%f\t%f", p1.x, p1.y);
-    calculate(&p2);
-    printf("\n%f\t%f", p2.x, p2.y);
-    check(&p1);
-    check(&p2);
 
-
-    for(int i = 0; i < 4; i++)
-    {
-    printf("\np1:%d", p1.binary[i]);
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("\np2:%d", p2.binary[i]);
-    }
-
-    test();
-
+ glClear(GL_COLOR_BUFFER_BIT);
+ glBegin(GL_POINTS);
+ ddaline(xmin,ymin,xmax,ymin);
+ ddaline(xmax,ymin,xmax,ymax);
+ ddaline(xmax,ymax,xmin,ymax);
+ ddaline(xmin,ymax,xmin,ymin);
+ ddaline(org_xa,org_ya,org_xb,org_yb);
+ 
+ glColor3f(1,0,0);
+ ddaline(xa , ya , xb ,yb);
+ 
+ glEnd();
+ glFlush();
 }
 
-void test()
+int main(int argc,char **argv)
 {
-  int i = 0, flag, flag1;
-
-  for (i = 0; i < 4; i++)
-  {
-      if (p1.binary[i] == 0 && p2.binary[i] == 0)
-      {
-          flag = 0;
-      }
-      else
-      {
-          flag = 1;
-          break;
-      }
-  }
-
-  if (flag == 0)
-  {
-      printf("\nAccepted:");
-  }
-  else
-  {
-      for (i = 0; i < 4; i++)
-      {
-          code[i] = p1.binary[i] && p2.binary[i];
-      }
-
-      for (i = 0; i < 4; i++)
-      {
-          if (code[i] == 1)
-          {
-              flag1 = 0;
-              break;
-          }
-          else
-          {
-              flag1 = 1;
-          }
-      }
-
-      if (flag1 == 0)
-      {
-          printf("\nRejected:");
-      }
-      else
-      {
-          printf("\nPartially accepted:");
-          find();
-      }
-  }
-
+	printf("Enter the window size :\n");
+	printf("Enter the value of Xmin: ");
+	scanf("%f",&xmin);
+	printf("Enter the value of Ymin: ");
+	scanf("%f",&ymin);
+	printf("Enter the value of Xmax: ");
+	scanf("%f",&xmax);
+	printf("Enter the value of Ymax: ");
+	scanf("%f",&ymax);
+	
+	printf("Enter the value of the start point:\n");
+	printf("X value :");
+	scanf("%f",&org_xa);
+	printf("Y value :");
+	scanf("%f",&org_ya);
+	printf("Enter the value of the end point:\n");
+	printf("X value :");
+	scanf("%f",&org_xb);
+	printf("Y value :");
+	scanf("%f",&org_yb);
+	
+	for (int i=0;i<4;i++)
+	{
+		outcode1[i]=0;
+		outcode2[i]=0;
+	}
+	
+	
+	if(org_ya > ymax)
+	{
+		outcode1[0]=1;	
+	}
+	
+	if(org_ya < ymin)
+	{
+		outcode1[1]=1;
+	}
+	
+	if(org_xa > xmax)
+	{
+		outcode1[2]=1;
+	}
+	
+	if(org_xa < xmin)
+	{
+		outcode1[3]=1;
+	}
+	
+	
+	if(org_yb > ymax)
+	{
+		outcode2[0]=1;	
+	}
+	
+	if(org_yb < ymin)
+	{
+		outcode2[1]=1;
+	}
+	
+	if(org_xb > xmax)
+	{
+		outcode2[2]=1;
+	}
+	
+	if(org_xb < xmin)
+	{
+		outcode2[3]=1;
+	}
+	
+	
+	printf("Outcodes are :\n");
+	for(int i=0 ; i<4 ; i++)
+	{
+		printf("\n%d\t%d",outcode1[i],outcode2[i]);
+		printf("\n");
+	}
+	
+	int count1=0;
+	for (int j=0;j<4 ;j++)
+	{
+		if(outcode1[j]==0 && outcode2[j]==0)
+		{
+			count1+=1;
+		}
+	}
+	if(count1==4)
+	{
+		printf("The line is accepted!!!!! ");
+		
+	}
+	else
+	{
+		int count2=0;
+		for (int j=0;j<4 ;j++)
+		{
+			if((outcode1[j] && outcode2[j])==0)
+			{
+				count2+=1;
+			}
+		}
+		if(count2==4)
+		{
+			printf("The line is partially accepted!!!!");
+			
+			slope=((org_yb-org_ya)/(org_xb-org_xa));
+			
+			if (outcode1[0]==1)
+			{
+				ya=ymax;
+				xa=org_xa+((ya-org_ya)/slope);
+			}
+			else if (outcode1[1]==1)
+			{
+				ya=ymin;
+				xa=org_xa+((ya-org_ya)/slope);
+			}
+			else if (outcode1[2]==1)
+			{
+				xa=xmax;
+				yb=org_ya+((xa-org_xa)*slope);
+			}
+			else if (outcode1[3]==1)
+			{
+				xa=xmin;
+				ya=org_ya+((xa-org_xa)*slope);
+			}
+			
+			else
+			{
+				xa=org_xa;
+				ya=org_ya;
+			}
+			
+			
+			if (outcode2[0]==1)
+			{
+				yb=ymax;
+				xb=org_xb+((yb-org_yb)/slope);
+			}
+			else if (outcode2[1]==1)
+			{
+				yb=ymin;
+				xb=org_xb+((yb-org_yb)/slope);
+			}
+			else if (outcode2[2]==1)
+			{
+				xb = xmax;
+				yb = org_yb+((xb-org_xb)*slope);
+			}
+			else if (outcode2[3]==1)
+			{
+				xb = xmin;
+				yb = org_yb+((xb-org_xb)*slope);
+			}
+			else
+			{
+				xb=org_xb;
+				yb=org_yb;
+			}
+			
+		}
+		else
+		{
+			printf("The line is rejected!!!!");
+		}
+	}
+	
+	 glutInit(&argc,argv); //It is used to initialise the GLUT library.
+	 glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//set the initial display mode.
+	 glutInitWindowPosition(0,0);
+	 glutInitWindowSize(640,480);
+	 glutCreateWindow("Scaling");
+	 glClearColor(0.0,0.0,0.0,0);
+	 glColor3f(0,0,1);
+	 gluOrtho2D(0,640,0,480);
+	 glutDisplayFunc(Draw);
+	 glutMainLoop();
+	return 0;
 }
-
-void Init()
-{
-  glClearColor(1.0, 1.0, 1.0, 0);
-  glColor3f(0.0, 0.0, 0.0);
-  gluOrtho2D(0, 640, 0, 480);
-}
-
-void Display()
-{
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  LineDDA(p1.x, p1.y, p2.x, p2.y);
-
-  LineDDA(xmin, ymin, xmax, ymin);
-  LineDDA(xmax, ymin, xmax, ymax);
-  LineDDA(xmax, ymax, xmin, ymax);
-  LineDDA(xmin, ymax, xmin, ymin);
-
-  check(&p1);
-  check(&p2);
-  test();
-
-  glColor3f(1.0, 0.0, 0.0);
-  LineDDA(p1.x, p1.y, p2.x, p2.y);
-
-}
-
-int main(int argc, char **argv)
-{
-  printf("\nEnter the xmin and ymin value of a rectangle:");
-  scanf("%d%d", &xmin, &ymin);
-  printf("\nEnter the xmax and ymax value of a rectangle:");
-  scanf("%d%d", &xmax, &ymax);
-  printf("\nEnter the x and y value of point p1:");
-  scanf("%f%f", &p1.x, &p1.y);
-  printf("\nEnter the x and y value of point p2:");
-  scanf("%f%f", &p2.x, &p2.y);
-
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowPosition(0, 0);
-  glutInitWindowSize(640, 480);
-  glutCreateWindow("Line Clipping");
-  Init();
-  glutDisplayFunc(Display);
-  glutMainLoop();
-
-}
-
